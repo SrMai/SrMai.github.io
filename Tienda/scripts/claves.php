a8bf501e2352a228053e8c9cefefc15f61751457
Carlos Ayala <49286868+SrMai@users.noreply.github.com>
2021-04-14 11:58:37 -0500

    Portafolio como principal
    Se añadio el portafolio como index principal del dominio

---
+++ Tienda/scripts/claves.php
@@ -0,0 +1,6 @@
+<?php
+$IP = 'localhost:7777';
+$NTienda = 'Crazy Games';
+$NTienda2 = 'Video Juegos Chespi';
+$InicialesT = 'VG';
+?>
--- scripts/claves.php
+++
@@ -1,6 +0,0 @@
-<?php
-$IP = 'localhost:7777';
-$NTienda = 'Crazy Games';
-$NTienda2 = 'Video Juegos Chespi';
-$InicialesT = 'VG';
-?>
